7294f2fc72e64739100a14357368e4a1d8124a13
Rasul <rasuldev@gmail.com>
2016-03-26 14:08:21 +0300

    chapter 9 is done

---
+++ CriminalIntent/app/src/main/java/com/bignerdranch/android/criminalintent/CrimeListActivity.java
@@ -0,0 +1,13 @@
+package com.bignerdranch.android.criminalintent;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by Rasul on 25.03.2016.
+ */
+public class CrimeListActivity extends SingleFragmentActivity {
+    @Override
+    protected Fragment createFragment() {
+        return new CrimeListFragment();
+    }
+}
