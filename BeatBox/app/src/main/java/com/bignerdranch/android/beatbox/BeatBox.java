026711c3418446f38026a2c0ea8b7491c2e8e978
Rasul <rasuldev@gmail.com>
2016-05-31 21:31:00 +0300

    chapter 19

--- BeatBox/app/src/main/java/com/bignerdranch/android/beatbox/BeatBox.java
+++ BeatBox/app/src/main/java/com/bignerdranch/android/beatbox/BeatBox.java
@@ -1,7 +1,10 @@
 package com.bignerdranch.android.beatbox;
 
 import android.content.Context;
+import android.content.res.AssetFileDescriptor;
 import android.content.res.AssetManager;
+import android.media.AudioManager;
+import android.media.SoundPool;
 import android.util.Log;
 
 import java.io.IOException;
@@ -14,13 +17,34 @@ import java.util.List;
 public class BeatBox {
     public static final String TAG = "BeatBox";
     public static final String SOUNDS_FOLDER = "sample_sounds";
+    public static final int MAX_SOUNDS = 5;
 
     private AssetManager mAssetManager;
     private List<Sound> mSounds = new ArrayList<>();
+    private SoundPool mSoundPool;
 
     public BeatBox(Context context) {
         mAssetManager = context.getAssets();
+        mSoundPool = new SoundPool(MAX_SOUNDS, AudioManager.STREAM_MUSIC, 0);
         loadSounds();
+    }
+
+    private void loadSound(Sound sound) throws IOException {
+        AssetFileDescriptor afd = mAssetManager.openFd(sound.getAssetPath());
+        int soundId = mSoundPool.load(afd, 1);
+        sound.setSoundId(soundId);
+    }
+
+    public void play(Sound sound) {
+        Integer soundId = sound.getSoundId();
+        if (soundId == null) {
+            return;
+        }
+        mSoundPool.play(soundId, 1f, 1f, 1, 0, 1f);
+    }
+
+    public void release() {
+        mSoundPool.release();
     }
 
     private void loadSounds() {
@@ -34,7 +58,13 @@ public class BeatBox {
         }
 
         for (String filename : soundNames) {
-            mSounds.add(new Sound(SOUNDS_FOLDER + "/" + filename));
+            Sound sound = new Sound(SOUNDS_FOLDER + "/" + filename);
+            try {
+                loadSound(sound);
+                mSounds.add(sound);
+            } catch (IOException e) {
+                Log.e(TAG, "Couldn't load sound " + filename, e);
+            }
         }
     }
 
